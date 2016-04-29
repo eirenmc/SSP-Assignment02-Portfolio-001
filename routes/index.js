1bce980322603a04c30388ee5b575253a55eea19
Eiren McLoughlin <eiren@gomobile.ie>
2016-04-29 14:30:36 +0100

    Still having trouble with Mongo and display
    Just after fixing it after it broke but still getting no results for mongo or displaying the images uploaded

--- routes/index.js
+++ routes/index.js
@@ -15,7 +15,7 @@ var username ="";
 
 // This is a variable that will either run the database from the set environment variables I have
 // set online or local host address
-//var url = process.env.CUSTOMCONNSTR_portfolioBuilderEiren || 'mongodb://localhost:27017/secretVaultData'; 
+//var url = process.env.CUSTOMCONNSTR_portfolioBuilderEiren || 'mongodb://localhost:27017/mongoDBAssignment02'; 
 
 /////////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -33,9 +33,48 @@ router.post('/uploadFile', function (req, res, next) {
   console.log(req.file);
 });
 
+router.post('loginAccount', function(req,res, next){
+    username = req.body.username;
+    password = req.body.password;
+});
+
 /* Renders the create portfolio piece page if button clicked */
 router.post('/projectEntry', function (req, res, next) {
   res.render('projectEntry', { title: 'Create a Portfolio Piece' });
+});
+
+router.post('projectEntry', function(req,res,next){
+    res.render('projectEntry');
+});
+
+/* Renders the login page if button clicked */
+router.get('/loginAccount', function (req, res, next) {  
+  res.render('loginAccount', { title: 'Login to manage your Portfolio' });
+  
+  console.log('The username is :' + username);
+  console.log('The username that was typed was' + username);
+});
+
+/* Renders the project list page if button clicked */
+router.get('/admin', function (req, res, next) {
+  res.render('admin', { title: 'Admin Page' });
+});
+
+/* Renders the admin page if button clicked */
+router.post('/admin', function (req, res, next) {
+  res.render('admin', { title: 'Manage your portfolio' });
+});
+
+/* Renders the project list page if button clicked */
+router.get('/projectList', function (req, res, next) {
+  res.render('projectList', { title: 'Portfolio Pieces' });
+});
+
+router.get('/logout', function(req, res, next) {
+  // To logout I simply destroy the session (and thus the username property on it)
+  req.session.destroy();
+  username = "";
+  res.render('login');  
 });
 
 /* Renders the create login page if button clicked */
@@ -53,38 +92,8 @@ router.post('/createAccount', function (req, res, next) {
   }
 });
 
-/* Renders the login page if button clicked */
-router.get('/loginAccount', function (req, res, next) {
-  
-  res.render('loginAccount', { title: 'Login to manage your Portfolio' });
-  console.log('The username is :' + username);
-  console.log('The username that was typed was' + username);
-});
-
 router.get('/createAccount', function (req, res, next) {
   res.render('createAccount', { title: 'Login to manage your Portfolio' });
-});
-
-router.get('/logout', function(req, res, next) {
-  // To logout I simply destroy the session (and thus the username property on it)
-  req.session.destroy();
-  username = "";
-  res.render('login');  
-});
-
-/* Renders the project list page if button clicked */
-router.get('/projectList', function (req, res, next) {
-  res.render('projectList', { title: 'Portfolio Pieces' });
-});
-
-/* Renders the project list page if button clicked */
-router.get('/admin', function (req, res, next) {
-  res.render('admin', { title: 'Admin Page' });
-});
-
-/* Renders the admin page if button clicked */
-router.post('/admin', function (req, res, next) {
-  res.render('admin', { title: 'Manage your portfolio' });
 });
 
 module.exports = router;
